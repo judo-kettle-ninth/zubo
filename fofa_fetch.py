d1839baac99dea4cfede02f520d329db7febd745
judo-kettle-ninth <judo-kettle-ninth@duck.com>
2025-12-31 14:36:36 +0800

    Update fofa_fetch.py

--- fofa_fetch.py
+++ fofa_fetch.py
@@ -9,7 +9,7 @@ from datetime import datetime, timezone, timedelta
 # ===============================
 # 配置区
 FOFA_URLS = {
-    "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBjb3VudHJ5PSJDTiIgJiYgcmVnaW9uPSJHdWFuZ2RvbmciICYmIGFzbj0iOTgwOCI%3D": "ip.txt",
+    "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBjb3VudHJ5PSJDTiIgJiYgcmVnaW9uPSJHdWFuZ2Rvbmci": "ip.txt",
 }
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
